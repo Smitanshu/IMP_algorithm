708f72095c44bd482cfd5962208dd130f158103a
Smitanshu <150527585+Smitanshu@users.noreply.github.com>
2024-04-15 20:07:36 +0530

    extra code added

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+
+    // int marks;
+    // string name;
+    // int id;
+    // void study(){
+    //     cout<<"Studying"<<endl;
+
+    // }
+    // void bunk(){
+    //     cout<<"Bunking"<<endl;
+    // }
+    // void sleep(){
+
+    //     cout<<"Sleeping"<<endl;
+    // }
