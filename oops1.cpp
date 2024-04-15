5d4253689dd62bee0266b351e02dd9fc3bb68753
Smitanshu <150527585+Smitanshu@users.noreply.github.com>
2024-04-15 19:17:38 +0530

    Basic OOPS concepts:

---
+++ oops1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Student {
+
+    int marks;
+    string name;
+    int id;
+    void study(){
+        cout<<"Studying"<<endl;
+
+    }
+    void bunk(){
+        cout<<"Bunking"<<endl;
+    }
+    void sleep(){
+        cout<<"Sleeping"<<endl;
+    }
+
+};
+int main(){
+
+    Student s1;
+    Student s2;
+}
