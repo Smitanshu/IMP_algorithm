ca4c3add542766fc52ebe5fc01e2fb23c0a1fb0b
Smitanshu <150527585+Smitanshu@users.noreply.github.com>
2024-04-15 20:38:48 +0530

    added cases

--- oops1.cpp
+++ oops1.cpp
@@ -58,7 +58,9 @@ class Student
     double marks;// It give (8+8+1+1+(6))==24;
     double id;
     char a;
-    bool b;// Basically it takes the nearest multiple if larger size datatype.
+    bool b;
+    //or Align the sum of datatype in the boundary of largest datatype
+    // Basically it takes the nearest multiple if larger size datatype.
 };
 int main(){
     cout<<sizeof(Student)<<endl;
