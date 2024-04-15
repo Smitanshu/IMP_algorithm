2a2cee33138ba6de36bf195a357c5c9bbcba3e73
Smitanshu <150527585+Smitanshu@users.noreply.github.com>
2024-04-15 22:50:09 +0530

    Basic access Specifier

---
+++ oops2.cpp
@@ -0,0 +1,22 @@
+// Access Modifier:Default members of a class is private
+#include <iostream>
+#include <string>
+using namespace std;
+class Student
+{
+public:
+    int id;
+    string name;
+    int marks;
+    int roNo;
+};
+
+int main()
+{
+    Student s1;
+    s1.name = "Smitanshu";  
+    cout << s1.name << endl;
+    //It Gives error if data memebr "name " is declared in private
+
+    return 0;
+}
