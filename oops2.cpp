9ca88b113b6fbc9e8f232abb33085f1c0110f3a3
Smitanshu <150527585+Smitanshu@users.noreply.github.com>
2024-04-16 22:38:13 +0530

    Added code

--- oops2.cpp
+++ oops2.cpp
@@ -2,22 +2,64 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class Student
+// class Student
+// {
+// public:
+//     int id;
+//     string name;
+//     int marks;
+//     int roNo;
+// };
+
+// int main()
+// {
+//     Student s1;
+//     s1.name = "Smitanshu";
+//     cout << s1.name << endl;
+//     //It Gives error if data memebr "name " is declared in private
+
+//     return 0;
+// }
+
+// Constructor:Which has same function name as class name.
+// It is an instance of an class which does not have return type.
+// Also called ctor it is the onlu function in cpp which does not have return type
+//If you dont write it default present.
+//if you write by custom
+//It runs only when object is created in main function.
+
+class Constructor
 {
+
 public:
     int id;
     string name;
     int marks;
-    int roNo;
+    bool present;
+   
+   Constructor(){
+    cout<<"It runs deafault due to it is present in the constructor funct";
+
+   }
+
+
+    void study(){
+        cout<<"Study";
+    }
+      void mark(){
+        cout<<"Marking";
+    }
+      void prsent(){
+        cout<<"Present";
+    }
 };
+int main(){
+   // Constructor c1;
+    //If object is not created or initialized in the main function.
+    //Ctor not execute .
 
 
-int main()
-{
-    Student s1;
-    s1.name = "Smitanshu";  
-    cout << s1.name << endl;
-    //It Gives error if data memebr "name " is declared in private
 
-    return 0;
+
+return 0;
 }
