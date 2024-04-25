722ffdbfa97b552e0d5871e0c490667a91ef2c29
Smitanshu <150527585+Smitanshu@users.noreply.github.com>
2024-04-26 01:40:53 +0530

    Modified

---
+++ OOP/oops5.cpp
@@ -0,0 +1,49 @@
+// Theory of Object oriented programming:
+#include <iostream>
+using namespace std;
+class Temp
+{
+};
+int main()
+{
+
+    return 0;
+}
+/*
+Pillars of OOPS:  main pillar={Abstraction}, {Loose coupling};
+
+1.Abstraction:Implementation details are hide is called abstraction.
+->Ex:We dont care the implementation of ATM,like HOW ?, what ?,   why?.
+->part of abstraction is called loose coupling:
+
+
+2.Encapsulation:Encapsulation is often mean to achieve abstraction  by hiding internal details
+and only exposing what is  necessary.
+
+->Wrapping or bundling of data and methods.
+->It is a way to achieve  or implement abstraction.
+->Why:
+    Easy to handle
+    Protect integrity.
+    control how class data is modified.
+->Perfect Encapsulation:If all data members are private.
+     Through getter /setter we can access.
+
+
+
+3.Inheritance:
+->  SuperClass || ParentClass|| BaseClass
+       ->>>>>>>
+       ->>>>>>>
+       subClass || childClass || DerivedClass
+
+->Syntax:
+    class Child_Name :Mode_of_Inheritance Parent_Name{....}
+    where:Mode_of_Inheritance=public, private, protected.
+
+->Code Reusable, increase code extensibility.
+
+
+->
+
+*/
